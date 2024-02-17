c75e976b99eeb61c9e502e73d3fb09d1c277071c
SK Arbaj Ali <skarbajali.one@outlook.com>
2024-02-17 16:40:25 +0530

    Fix: CORS error handled.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const graphqlHTTP = require('express-graphql');
 const connectDB = require('./db');
 const Schema = require('./schema/schema');
 
 const app = express();
+
+app.use(cors());
 
 connectDB(process.env.MONGODB_URI, process.env.DB_NAME);
 
